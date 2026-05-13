8ec69d7b00bb991e29e6f45b825b29fda5807177
cesarvb-0 <cesarluisvb2007@gmail.com>
2026-05-13 09:47:58 -0500

    se definio la funcion obtener_cursor

--- src/controller/empleado_controller.py
+++ src/controller/empleado_controller.py
@@ -4,6 +4,13 @@ from model.empleados import empleados
 
 
 class controlador:
+    def obtener_cursor():
+        coneccion = psycopg2.connect(database="calculadora_liquidacion_qjii" , user="admin" , password="5UGfOj4KELdPGu3vJgc74cE5aiMB3cVT" , host="dpg-d7uv3g3eo5us73ddtga0-a.oregon-postgres.render.com" , port="5432" )
+
+        cursor = coneccion.cursor()
+
+        return cursor
+
     def insertar(empleado: empleados):
         coneccion = psycopg2.connect(database="calculadora_liquidacion_qjii" , user="admin" , password="5UGfOj4KELdPGu3vJgc74cE5aiMB3cVT" , host="dpg-d7uv3g3eo5us73ddtga0-a.oregon-postgres.render.com" , port="5432" )
 
@@ -18,3 +25,4 @@ class controlador:
     def buscar(salario: int):
         coneccion = psycopg2.connect(database="calculadora_liquidacion_qjii" , user="admin" , password="5UGfOj4KELdPGu3vJgc74cE5aiMB3cVT" , host="dpg-d7uv3g3eo5us73ddtga0-a.oregon-postgres.render.com" , port="5432" )
 
+    
