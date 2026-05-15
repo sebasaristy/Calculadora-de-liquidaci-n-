7bc755be5beaddcb0f9786e72acd73569f00d65e
Sexar <cesarluisvb2007@gmail.com>
2026-05-15 09:41:12 -0500

    html sexar

--- app_sexar.py
+++ app_sexar.py
@@ -6,16 +6,8 @@ app = Flask(__name__)
 #Por cada ruta que vayamos a atender en el navegador, creamos una funcion en Python
 @app.route("/") #El decordador indica la ruta que llama a esta funcion
 
-def hola():
-    #Lo que la funcion retorne, llega en el cuerpo HTML al navegador
-    return "<strong><h1>Hola Mundo</h1></strong>"
-@app.route("/clase")
-def hola_clase():
-    return "<strong><h1>Hola Clase</h1></strong>"
-
-@app.route("/tim")
 def tim():
-    return render_template("hola.html")
+    return render_template("sexar_ht.html")
 
 
 app.run(debug=True)
