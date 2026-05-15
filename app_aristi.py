1e86ad701e0e6c147b8975a5f7219f8b3f146ce3
Sebastian <sebasaristy.0809@gmail.com>
2026-05-15 09:41:27 -0500

    html arixti

---
+++ app_aristi.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+# Crear un instancia de Flask que sera nuestra aplicacion
+app = Flask(__name__)
+
+#Por cada ruta que vayamos a atender en el navegador, creamos una funcion en Python
+@app.route("/") #El decordador indica la ruta que llama a esta funcion
+
+def tim():
+    return render_template("aristi_ht.html")
+
+
+app.run(debug=True)
